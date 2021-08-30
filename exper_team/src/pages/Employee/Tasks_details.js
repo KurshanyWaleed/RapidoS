3fbdf4079c173f16d42c7b8012ac149d5d051bb3
kurshanyWaleed <kurshany.waleed@gmail.com>
2021-08-30 18:50:22 +0100

    v.3.5.0 info

--- exper_team/src/pages/Employee/Tasks_details.js
+++ exper_team/src/pages/Employee/Tasks_details.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 import jwtDecode from "jwt-decode";
 import { useParams } from 'react-router-dom';
 import Map from '../../components/Maps';
+import { Button, Row, Col } from 'react-bootstrap';
 const Tasks_details = () => {
     const { id } = useParams();
     const decryptedId = jwtDecode(id);
@@ -12,19 +13,35 @@ const Tasks_details = () => {
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
             <Map lat={lat} lng={lng} />
-            <div>
-                <p>
-                    details id :{decryptedId.id}
-                </p>
-                <p>
-                    details name :{decryptedId.name}
-                </p>
-                <p>
-                    details type :{decryptedId.type}
-                </p>
-                <p>
-                    details town :  {decryptedId.town}
-                </p>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', width: '50%', margin: '10px' }}>
+                <span>
+                    <b>
+                        Pour Mr/Mdme :
+                    </b>
+                    {decryptedId.name}
+                </span>
+                <span>
+                    <b>
+                        Type :
+                    </b>
+                    {decryptedId.type}
+                </span>
+                <span>
+                    <b>
+                        Ville :
+                    </b>
+                    {decryptedId.town}
+                </span>
+
+
+            </div>
+            <div style={{ width: '50%' }}>
+
+                <button style={{ margin: '5px' }} type="button" class="btn btn-outline-success">Accepter la Mession</button>
+                <button style={{ margin: '5px' }} type="button" class="btn btn-outline-danger">Signaler un probleme</button>
+                <button style={{ margin: '5px' }} type="button" class="btn btn-outline-warning">Reporter</button>
+                <button type="button" class="btn btn-outline-info">Imprimer</button>
+
             </div>
         </div>
     )
